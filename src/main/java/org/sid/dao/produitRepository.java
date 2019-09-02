7447b7a87de7fe08d8991d05dc2d7c2bd3ee81ba
mouhamed92 <hamamimohamed92@gmail.com>
2019-09-02 13:01:33 +0100

    seconde commit

--- src/main/java/org/sid/dao/produitRepository.java
+++ src/main/java/org/sid/dao/produitRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 import org.sid.entities.Produit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface produitRepository extends JpaRepository<Produit, Long> {
 	@Query("select p from Produit p where designation like:x")
-	public List<Produit>chercher(@Param("x")String mc);
+	public Page<Produit>chercher(@Param("x")String mc , Pageable pagebale );
 
 }
--- src/main/java/org/sid/web/ProduitControlleur.java
+++ src/main/java/org/sid/web/ProduitControlleur.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.sid.dao.produitRepository;
 import org.sid.entities.Produit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +20,14 @@ public class ProduitControlleur {
 	
 	@RequestMapping(value="/index")
 	private String index(Model model,
+					      @RequestParam(name="page" ,defaultValue="0")int p,	
+						  @RequestParam(name="size" ,defaultValue="5")int s,
 			              @RequestParam(name="motCle" ,defaultValue="")String mc){
 		
-		List<Produit> produit =   produitrep.chercher("%"+mc+"%");
+		Page<Produit> produit =   produitrep.chercher("%"+mc+"%",new PageRequest(p, s));
+		int[] pages = new int[produit.getTotalPages()];
 		model.addAttribute("produits",produit);
+		model.addAttribute("pages",pages);
 		model.addAttribute("motCle",mc);
 		
 		return "index";
